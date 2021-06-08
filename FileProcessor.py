965e80fdc0cc9e039e211279f3291aaa94a7747a
Christelle Randriamahefa <nomentsoa.randriamahefa@etu.univ-savoie.fr>
2021-06-09 00:28:49 +0200

    PEP8

--- FileProcessor.py
+++ FileProcessor.py
@@ -1,18 +1,32 @@
 class FileProcess:
+
+    """Constructeur de la classe FileProcess.
+    Mis en commentaire par la suite car il imposait une contrainte supplementaire dans les autres classes.
+    """
     #def __init__(self, filePath):
         #name of the file that is going to be processed
     #  self.filePath = filePath
 
+    """Retourne le contenu d'un fichier dont le nom est en parametre.
+    Fonctions utilisées:
+        - open() : ouvre filePath et donne un objet fichier correspondant
+        - read() : renvoie le nombre d'octets associés au fichier
+    """
     #takes in parameter a file path and returns the content of the file
     def getFileContent(self, filePath):
         return open(filePath).read()
 
+    """Renvoie tous les caracteres du fichier couples a leur code ASCII.
+        - ord() : renvoie l'entier representant l'Unicode du caractere
+    """
     #returns each caracter and its ASCII code
     def ASCII_extractor(self, filePath):
         fileContent = self.getFileContent(filePath)
         for c in fileContent:
             print(c, ord(c))
 
+    """Retourne l'alphabet associe au texte ainsi que la frequence de chaque caractere.
+    """
     #returns the alphabet and the caracters' individual frequency in a txt file
     def getAlphabetandFreq(self, filePath):
         fileContent = self.getFileContent(filePath)
@@ -27,6 +41,8 @@ class FileProcess:
                 frequency[position] = frequency[position] + 1
         return alphabet, frequency
 
+    """Prend en parametre l'alphabet et la frequence et retourne un dictionnaire dic.
+    """
     #returns a dictionary that contains the alphabet and the caracters' individual frequency
     def dictionary(self, alphabet, frequency):
         #creation of a new dictionary
@@ -35,22 +51,15 @@ class FileProcess:
             dic[frequency[k]] = alphabet[k]
         return dic
 
-    #returns the newly created dictionary sorted by caracter frequency
-    def freqSort(self, dic):
-        dicByFreq = {i: v for i, v in sorted(dic.items(), key=lambda item: item[1])}
-        return dicByFreq
-
-    #returns the newly created dictionary sorted by ASCII code
-    def ASCIISort(self, dic):
-        dicByASCII = dict()
-        for k in sorted(dic.keys()):
-            dicByASCII[k] = dic[k]
-        return dicByASCII
-
+    """Effectue un tri du dictionnaire passé en paramètre.
+    Le tri est d'abord réalisé par fréquence puis par code ASCII.
+    """
     #sorts the newly created dictionary by frequency, then by ASCII code
     def freqThenASCII_sort(self, dic):
         return sorted(dic.items(), key = lambda x: (x[1],x[0]))
-
+    
+    """Crée un nouveau fichier txt qui se terminera par _Frequency.
+    """
     #creates a new txt file that contains each caracter and its frequency
     def writeFreqInFile(self, caracter, freq, fileName):
         with open(fileName[:4] + '_Frequency.txt', 'a') as f:
