e48b97f4d79c7c58c0b6a4a0e503c5b2c4e97f83
Christelle Randriamahefa <nomentsoa.randriamahefa@etu.univ-savoie.fr>
2021-06-08 19:48:22 +0200

    Final

--- BinaryProcessor.py
+++ BinaryProcessor.py
@@ -12,7 +12,26 @@ class BinaryProcess:
         while len(content)%8 != 0:
             content = content + "0"
 
+    #returns a list of the binary codes associated with the caracters
     def createBinList(self, content):
         BinList = []
         for i in range (8, len(content), 8):
             BinList.append(content[i-8:i])
+        return BinList
+
+    #creates a bin file
+    def writeBinFile(self, fileName, content):
+        with open(fileName, "wb") as f:
+            for i in content:
+                f.write((int(i, base=2)).to_bytes((len(i)//8), byteorder='big'))
+        f.close()
+    
+    #writes a txt file containing the binary codes assigned to each caracter
+    def writeBinCodes(self, alphabet, binTable, fileName):
+        with open(fileName[:4]+'_BinCodes.txt','a') as f:
+            for i in alphabet:
+                f.write("The binary code of " + i + "is : " + str(binTable[i]) + '\n')
+            f.close()
+
+
+
