7ab98da510329f0d188b4a513956481e4be69f13
Christelle Randriamahefa <72504027+christelle101@users.noreply.github.com>
2021-06-08 19:40:13 +0200

    To be PEP8

--- AvgBits.py
+++ AvgBits.py
@@ -1,19 +1,24 @@
 class AverageStorageBits:
 
+    #constructor
     def __init__(self, binAlphabet: dict, frequencies: dict, initialFile:str):
         self.binAlphabet = binAlphabet
         self.frequencies = frequencies
         self.initialFile = initialFile
-        
+    
+    #returns the binary alphabet
     def getBinAlphabet(self):
         return self.binAlphabet
 
+    #returns the text frequencies
     def getFrequencies(self):
         return self.frequencies
 
+    #returns the initial file
     def getInitialFile(self):
         return self.initialFile
 
+    #returns the average number of storage bits
     def calculateAvgStorage(self):
         total = 0
         binTree = self.getBinAlphabet()
