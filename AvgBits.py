2713b43a41772231f91239ce3108108ba3a2ecc1
Christelle Randriamahefa <72504027+christelle101@users.noreply.github.com>
2021-06-13 11:48:59 +0200

    Final

--- AvgBits.py
+++ AvgBits.py
@@ -1,23 +1,36 @@
 class AverageStorageBits:
 
+    """ Constructeur de la classe AverageStorageBits.
+    -   binAlphabet : alphabet binaire
+    -   frequencies : fréquences d'apparition
+    -   initialFile : fichier initial
+    """
     #constructor
     def __init__(self, binAlphabet: dict, frequencies: dict, initialFile:str):
         self.binAlphabet = binAlphabet
         self.frequencies = frequencies
         self.initialFile = initialFile
     
+    """ Retourne l'alphabet binaire.
+    """
     #returns the binary alphabet
     def getBinAlphabet(self):
         return self.binAlphabet
 
+    """ Retourne les fréquences d'apparition.
+    """
     #returns the text frequencies
     def getFrequencies(self):
         return self.frequencies
 
+    """ Retourne le fichier initial.
+    """
     #returns the initial file
     def getInitialFile(self):
         return self.initialFile
 
+    """ Retourne le nombre moyen de bits de stockage.
+    """
     #returns the average number of storage bits
     def calculateAvgStorage(self):
         total = 0
