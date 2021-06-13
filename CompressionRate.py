d2680f0512d4b81447dce9b4c7441956819369d0
Christelle Randriamahefa <72504027+christelle101@users.noreply.github.com>
2021-06-13 12:37:16 +0200

    Final

--- CompressionRate.py
+++ CompressionRate.py
@@ -1,17 +1,18 @@
+""" Permet de calculer le taux de compression.
+"""
 class CompressionRate:
 
-    """ Constructeur de la classe CompressionRate.
+    """ Constructeur de la classe compression_rate.
     -   binAlphabet : alphabet binaire
     -   frequencies : fréquences
     -   initialFile : fichier initial
     -   savedRatio : taux d'espace gagné
     """
-    #constructor
-    def __init__(self, binAlphabet: dict, frequencies: dict, initialFile:str):
-        self.binAlphabet = binAlphabet
+    def __init__(self, bin_alphabet: dict, frequencies: dict, initial_file:str):
+        self.bin_alphabet = bin_alphabet
         self.frequencies = frequencies
-        self.initialFile = initialFile
-        self.savedRatio = self.calculateRatio()
+        self.initial_file = initial_file
+        self.saved_ratio = self.calculate_ratio()
 
     """ Getters et setters.
     Fonctions qui permettent la modification et l'accession de certains attributs de l'objet.
@@ -19,36 +20,31 @@ class CompressionRate:
 
     """ Retourne l'alphabet binaire.
     """
-    #returns the binary alphabet
-    def getBinAlphabet(self):
-        return self.binAlphabet
+    def get_bin_alphabet(self):
+        return self.bin_alphabet
 
     """ Retourne les fréquences d'apparition.
     """
-    #returns the text frequencies
-    def getFrequencies(self):
+    def get_frequencies(self):
         return self.frequencies
 
     """ Retourne le fichier initial.
     """
-    #returns the initial file
-    def getInitialFile(self):
-        return self.initialFile
+    def get_initial_file(self):
+        return self.initial_file
 
     """ Retourne le taux de données gagnées.
     """
-    #returns the percentage of saved data
-    def getSavedRatio(self):
-        return self.savedRatio
+    def get_saved_ratio(self):
+        return self.saved_ratio
 
     """ Retourne le taux de compression personnalisé pour un fichier donné
     """
-    #returns the file compression ratio
-    def calculateRatio(self):
-        compresionCount = 0
-        initialCount = len(self.getInitialFile())*8
+    def calculate_ratio(self):
+        compresion_count = 0
+        initial_count = len(self.get_initial_file())*8
 
-        for caracter in self.getFrequencies():
-            compresionCount += int(len(self.getBinAlphabet()[caracter[0]] * caracter[1]))
-        print(compresionCount, initialCount)
-        return 1 - float(compresionCount/initialCount)
+        for caracter in self.get_frequencies():
+            compresion_count += int(len(self.get_bin_alphabet()[caracter[0]] * caracter[1]))
+        print(compresion_count, initial_count)
+        return 1 - float(compresion_count/initial_count)
