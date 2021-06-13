457e2e0e88b50b9c05cf272975bfe1378b22aa98
Christelle Randriamahefa <72504027+christelle101@users.noreply.github.com>
2021-06-13 12:35:29 +0200

    Final

--- FileProcessor.py
+++ FileProcessor.py
@@ -1,44 +1,36 @@
+""" Procède au traitement du fichier initial.
+"""
 class FileProcess:
-
-    """Constructeur de la classe FileProcess.
-    Mis en commentaire par la suite car il imposait une contrainte supplementaire dans les autres classes.
-    """
-    #def __init__(self, filePath):
-        #name of the file that is going to be processed
-    #  self.filePath = filePath
 
     """Retourne le contenu d'un fichier dont le nom est en parametre.
     Fonctions utilisées:
-        - open() : ouvre filePath et donne un objet fichier correspondant
+        - open() : ouvre file_path et donne un objet fichier correspondant
         - read() : renvoie le nombre d'octets associés au fichier
     """
-    #takes in parameter a file path and returns the content of the file
-    def getFileContent(self, filePath):
-        return open(filePath).read()
+    def get_file_content(self, file_path):
+        return open(file_path).read()
 
     """Renvoie tous les caracteres du fichier couples a leur code ASCII.
     Fonctions utilisées :
         - ord() : renvoie l'entier representant l'Unicode du caractere
     """
-    #returns each caracter and its ASCII code
-    def ASCII_extractor(self, filePath):
-        fileContent = self.getFileContent(filePath)
-        for c in fileContent:
+    def ascii_extractor(self, file_path):
+        file_content = self.get_file_content(file_path)
+        for c in file_content:
             print(c, ord(c))
 
     """Retourne l'alphabet associe au texte ainsi que la frequence de chaque caractere.
     """
-    #returns the alphabet and the caracters' individual frequency in a txt file
-    def getAlphabetandFreq(self, filePath):
-        fileContent = self.getFileContent(filePath)
+    def get_alpha_and_freq(self, file_path):
+        file_content = self.get_file_content(file_path)
         alphabet = []
         frequency = []
-        for i in range(0, len(fileContent)):
-            if fileContent[i] not in alphabet:
-                alphabet.append(fileContent[i])
+        for i in range(0, len(file_content)):
+            if file_content[i] not in alphabet:
+                alphabet.append(file_content[i])
                 frequency.append(1)
             else:
-                position = alphabet.index(fileContent[i])
+                position = alphabet.index(file_content[i])
                 frequency[position] = frequency[position] + 1
         return alphabet, frequency
 
@@ -46,9 +38,7 @@ class FileProcess:
     Fonctions utilisées :
     -   dict() : crée un dictionnaire
     """
-    #returns a dictionary that contains the alphabet and the caracters' individual frequency
     def dictionary(self, alphabet, frequency):
-        #creation of a new dictionary
         dic = dict()
         for k in range(0, len(alphabet)):
             dic[frequency[k]] = alphabet[k]
@@ -59,19 +49,18 @@ class FileProcess:
     Fonctions utilisées :
     -   sorted() : renvoie une liste triée
     """
-    #sorts the newly created dictionary by frequency, then by ASCII code
-    def freqThenASCII_sort(self, dic):
+    def freq_then_ascii_sort(self, dic):
         return sorted(dic.items(), key = lambda x: (x[1],x[0]))
-    
+
     """Crée un nouveau fichier txt qui se terminera par _Frequency.
     Fonctions utilisées :
-    -   open() : ouvre fileName et donne un fichier correspondant
+    -   open() : ouvre file_name et donne un fichier correspondant
     -   close() : ferme le fichier ouvert
     """
     #creates a new txt file that contains each caracter and its frequency
-    def writeFreqInFile(self, caracter, freq, fileName):
-        with open(fileName[:4] + '_Frequency.txt', 'a') as f:
-            if (freq != None):
+    def write_freq_in_file(self, caracter, freq, file_name):
+        with open(file_name[:4] + '_Frequency.txt', 'a') as f:
+            if freq != None:
                 f.write(caracter + '' + str(freq) + '\n')
             else:
                 f.write(caracter + '\n')
